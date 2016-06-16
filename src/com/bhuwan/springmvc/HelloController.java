ab5e245539b0a3334b8666bb0e6d20eda86a71f4
bhuwang <bhuwangautam@lftechnology.com>
2016-06-16 09:13:12 +0545

    using_parameterizable_view_controller

--- src/com/bhuwan/springmvc/HelloController.java
+++ src/com/bhuwan/springmvc/HelloController.java
@@ -10,21 +10,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
+import org.springframework.web.servlet.mvc.ParameterizableViewController;
 
 /**
  * @author bhuwan
  *
  */
-public class HelloController extends AbstractController {
+public class HelloController extends ParameterizableViewController {
 
     @Override
     protected ModelAndView handleRequestInternal(HttpServletRequest req, HttpServletResponse res) throws Exception {
-        System.out.println("Inside Hello Controller which extends AbstractController........");
+        System.out.println("Inside Hello Controller which extends ParameterizableViewController........");
         String name = req.getParameter("name");
         Map<String, String> map = new HashMap<String, String>();
         map.put("greetings", "Hello " + name + "!");
-        return new ModelAndView("hello", map);
+        return new ModelAndView(getViewName(), map);
     }
 
 }
